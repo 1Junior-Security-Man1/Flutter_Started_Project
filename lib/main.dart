3c07c6a28680a08e42a7021f144a10e62761111b
Artem Shkryab <artem.shkriab@lindenvalley.de>
2020-11-05 15:50:18 +0200

    localization

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,42 @@ import 'package:bounty_hub_client/data/repositories/campaigns_repository.dart';
 import 'package:bounty_hub_client/data/repositories/login_repository.dart';
 import 'package:bounty_hub_client/data/repositories/tasks_repository.dart';
 import 'package:bounty_hub_client/data/repositories/user_repository.dart';
+import 'package:bounty_hub_client/utils/localization/bloc/locale_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'app.dart';
-import 'bloc/authentication_event.dart';
-import 'bloc/authorization_bloc.dart';
+import 'bloc/auth/authentication_event.dart';
+import 'bloc/auth/authorization_bloc.dart';
 import 'network/server_api.dart';
 
 void main() {
   final RestClient client = RestClient();
 
-  runApp(MultiRepositoryProvider(providers: [
-    // provide repositories here
-    RepositoryProvider<UserRepository>(create: (context) => UserRepository()),
-    RepositoryProvider<CampaignRepository>(create: (context) => CampaignRepository(client)),
-    RepositoryProvider<LoginRepository>(create: (context) => LoginRepository(client)),
-    RepositoryProvider<TaskRepository>(create: (context) => TaskRepository(client)),
-
-  ], child: MultiBlocProvider(providers: [
-    BlocProvider(create: (context) => AuthenticationBloc(UserRepository())..add(AppStarted())),
-    // provide App blocs here
-  ], child: App())));
+  runApp(MultiRepositoryProvider(
+      providers: [
+        // provide repositories here
+        RepositoryProvider<UserRepository>(
+            create: (context) => UserRepository()),
+        RepositoryProvider<CampaignRepository>(
+            create: (context) => CampaignRepository(client)),
+        RepositoryProvider<LoginRepository>(
+            create: (context) => LoginRepository(client)),
+        RepositoryProvider<TaskRepository>(
+            create: (context) => TaskRepository(client)),
+      ],
+      child: MultiBlocProvider(providers: [
+        BlocProvider(
+            create: (context) =>
+                AuthenticationBloc(UserRepository())..add(AppStarted())),
+        BlocProvider(create: (context) => LocaleBloc()),
+        // provide App blocs here
+      ], child: App())));
   appConfig();
 }
 
 void appConfig() {
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitDown,DeviceOrientation.portraitUp]);
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
 }
---
+++ lib/utils/localization/bloc/locale_bloc.dart
@@ -0,0 +1,38 @@
+import 'package:bounty_hub_client/data/repositories/locale_repository.dart';
+import 'package:bounty_hub_client/utils/localization/bloc/locale_event.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class LocaleBloc extends Bloc<LocaleEvent, Locale> {
+  LocaleBloc() : super(const Locale('en', 'EN'));
+  static Locale locale = Locale('en', 'EN');
+
+  @override
+  Stream<Locale> mapEventToState(LocaleEvent event) async* {
+
+    if (event is ChangeLocaleEvent) {
+      LocaleRepository().setLanguage(
+          languageCode: event.languageCode, countryCode: event.countryCode);
+      var locale = Locale(event.languageCode, event.countryCode);
+      LocaleBloc.locale = locale;
+      try {
+        //todo set language API
+      }catch(e){
+        //
+      }
+      yield locale;
+    }
+
+    if (event is LoadSavedLocale) {
+      yield _fetchLocale();
+    }
+  }
+
+  Locale _fetchLocale() {
+    if (LocaleRepository().language == null) {
+      return const Locale('en', 'EN');
+    }
+    locale = Locale(LocaleRepository().language, LocaleRepository().language.toUpperCase());
+    return locale;
+  }
+}
