0cb3f9beab48d5501696ac76d1a02da8a1018314
Nikolay Popov <np@lindenvalley.de>
2021-02-08 15:36:09 +0200

    added new place holder widget for empty data

--- lib/ui/widgets/empty_data_place_holder.dart
+++ lib/ui/widgets/empty_data_place_holder.dart
@@ -13,10 +13,20 @@ class EmptyDataPlaceHolder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Text(
-          message != null ? message :AppStrings.emptyDataMessage,
-          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: AppColors.textColor),
-        ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/place_holder.png',
+            width: 180,
+            height: 180,
+          ),
+          Text(
+            message != null ? message : AppStrings.emptyDataMessage,
+            style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600, color: AppColors.textColor),
+          ),
+         ],
+      ),
     );
   }
 }
