83eff7d9745576c991b9983501c89709379174e3
Nikolay Popov <np@lindenvalley.de>
2021-03-05 12:47:17 +0200

    enable no social mode for IOS only

--- lib/utils/bloc_utils.dart
+++ lib/utils/bloc_utils.dart
@@ -1,10 +1,12 @@
+import 'dart:io';
+
 import 'package:bounty_hub_client/app.dart';
 import 'package:bounty_hub_client/data/remote_app_data.dart';
 import 'package:bounty_hub_client/data/enums/app_types.dart';
 import 'package:bounty_hub_client/utils/locator.dart';
 
 // TODO hardcode solution. Block does not respond to the change of the enum values
-//  like ui/pages/authorization/cubit/authorization_state.dart:3 - like AuthorizationStatus etc.
+//  like ui/pages/authorization/cubit/authorization_state.dart:3 - like AuthorizationStatus enum.
 int generateSignature() {
   return DateTime.now().millisecondsSinceEpoch;
 }
@@ -12,7 +14,9 @@ int generateSignature() {
 // Method gets the values application mode and build version from firebase remote config,
 // this is needed for hide some UI components, for approval in the Apple Store Connect
 bool isNoSocialMode() {
-  return Platform.isIOS && locator<RemoteAppData>().appType == AppType.NO_SOCIAL && locator<RemoteAppData>().buildVersion == AppState.buildVersion;
+  return Platform.isIOS
+      && locator<RemoteAppData>().appType == AppType.NO_SOCIAL
+      && locator<RemoteAppData>().buildVersion == AppState.buildVersion;
 }
 
 /* Current store version: 1.0.12
